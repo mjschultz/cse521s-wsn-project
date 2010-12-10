981d8d52310a3cfd0440af2706b7dfcb4f76265e
Michael Schultz <mjschultz@poseidon.olympus>
2010-12-09 20:38:32 -0600

    - added a first pass to the aggregator section - modified simulator for testing

--- src/frontend/simulator.py
+++ src/frontend/simulator.py
@@ -4,6 +4,9 @@ import httplib
 import sys
 import random
 import json
+import time
+from datetime import datetime as dtdt
+from datetime import time as dtt
 
 if len(sys.argv) >= 2 :
     hostname = sys.argv[1]
@@ -19,17 +22,39 @@ def space_list(min, max, items) :
     for i in range(items) :
         yield random.randint(min,max)
 
-host = httplib.HTTPConnection(hostname)
 simulating = True
 while simulating :
+    host = httplib.HTTPConnection(hostname)
     print 'PUT to',hostname,'in lot',lotname,'...',
 
     # generage a random number of spaces to change
     spaces = []
-    changed = random.randint(1, 8)
-    for space_id in space_list(0, 10, changed) :
+    changed = random.randint(1, 10)
+    now = dtdt.now()
+    six_oclock = dtdt.combine(now.date(), dtt(6, 00))
+    seven_oclock = dtdt.combine(now.date(), dtt(7, 00))
+    twelve_oclock = dtdt.combine(now.date(), dtt(12, 00))
+    thirteen_oclock = dtdt.combine(now.date(), dtt(13, 00))
+    eighteen_oclock = dtdt.combine(now.date(), dtt(18, 00))
+    nineteen_oclock = dtdt.combine(now.date(), dtt(19, 00))
+    twentytwo_oclock = dtdt.combine(now.date(), dtt(22, 00))
+    five_oclock = dtdt.combine(now.date(), dtt(5, 00))
+    if six_oclock <= now <= seven_oclock :
+        weight = 3
+    elif twelve_oclock <= now <= thirteen_oclock :
+        changed *= 2
+        weight = 3
+    elif eighteen_oclock <= now <= nineteen_oclock :
+        weight = 3
+    elif now >= twentytwo_oclock or now <= five_oclock :
+        weight = 5
+        changed /= 2
+    else :
+        weight = 1
+
+    for space_id in space_list(0, 30, changed) :
         space = {'space_id': space_id,
-                 'is_empty': bool(random.randint(0,1))}
+                 'is_empty': bool(random.randint(0,weight))}
         for thing in ('temperature','sonar','light','magnet') :
             if bool(random.randint(0,1)) :
                 space[thing] = int(random.random()*256)
@@ -48,8 +73,8 @@ while simulating :
         del space['is_empty']
         print '\t'+str(space)
 
-    next_round = raw_input('Continue (Y/n): ')
-    if next_round not in ('', 'yes', 'y', 'Y') :
-        simulating = False
+    host.close()
+    time.sleep(90+random.randint(-30,30))
+    # sleeps between 60 and 120 seconds
 
 print 'Done.'
